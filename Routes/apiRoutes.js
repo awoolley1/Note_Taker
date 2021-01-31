83096c03fe6253991ec9b5552f18ffc58ed4c1fe
Local Al <alexander.s.woolley@gmail.com>
2021-01-30 19:38:56 -0700

    writeFileSync added

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,4 +1,10 @@
 var notesData = require("../db/db.json");
+const fs = require("fs");
+
+const util = require("util");
+//const writeFileAsync = util.promisify(fs.writeFile);
+//const readFileAsync = util.promisify(fs.readFile);
+
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
@@ -6,10 +12,10 @@ module.exports = function (app) {
   });
 
   app.post("/api/notes", function (req, res) {
-    
     console.log(req.body);
-      notesData.push(req.body);
-      res.json(true);
-    
+    notesData.push(req.body);
+    console.log(notesData)
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesData))
+    res.json(notesData)
   });
 };
